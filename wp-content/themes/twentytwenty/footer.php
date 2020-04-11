2834b3d13231cf45aea4a44fd36bb97220e4d252
Čeda <cedaivic@gmail.com>
2020-04-11 19:20:24 +0000

    Small change in footer - testing deploy to live repo

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -38,7 +38,10 @@
 
 					<a class="to-the-top" href="#site-header">
 						<span class="to-the-top-long">
-							<span class="arrow" aria-hidden="true">&uarr;</span>
+							<?php
+							/* translators: %s: HTML character for up arrow. */
+							printf( __( 'To the top %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
+							?>
 						</span><!-- .to-the-top-long -->
 						<span class="to-the-top-short">
 							<?php
